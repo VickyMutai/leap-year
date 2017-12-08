a13f23366baf81d797f1f93e81738a8576f54733
VickyMutai <vicky.mutai96@gmail.com>
2017-12-08 10:30:53 +0300

    Add changes to business logic

--- leap-year/js/script.js
+++ leap-year/js/script.js
@@ -1,12 +1,5 @@
 //business logic
 var leapYear = function(year){
-  /*if(year % 100===0){
-    return false;
-  }else if (year % 4 === 0){
-    return true
-  } else {
-    return false;
-  }*/
   if((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
   } else {
@@ -19,6 +12,13 @@ $(document).ready(function(){
     event.preventDefault();
     var year=parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+    if(!result){
+      $(".not").text("not");
+    }else {
+      $(".not").text("");
+    }
+    $("#result").show();
   });
 });
